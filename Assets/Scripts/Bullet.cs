cd45e4d259a8acd7ababcb345de974f7f643be12
Moisés Vilar <mvilar@gradiant.org>
2020-10-12 21:01:51 +0200

    Curso 3, unidad 6: utilidades varias

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,11 +4,22 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 2f;
     public Vector2 direction;
+    public float livingTime = 3f;
+    public Color initialColor = Color.white;
+    public Color finalColor;
+
+    private SpriteRenderer _renderer;
+    private float _startingTime;
+
+    void Awake() {
+        this._renderer = this.GetComponent<SpriteRenderer>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this._startingTime = Time.time;
+        Destroy(this.gameObject, this.livingTime);
     }
 
     // Update is called once per frame
@@ -17,5 +28,9 @@ public class Bullet : MonoBehaviour
         Vector2 movement = this.direction.normalized * this.speed * Time.deltaTime;
         // this.transform.position = new Vector2(this.transform.position.x + movement.x, this.transform.position.y + movement.y);
         this.transform.Translate(movement);
+
+        float _timeSinceStarted = Time.time - this._startingTime;
+        float _percentageCompleted = _timeSinceStarted / this.livingTime;
+        this._renderer.color = Color.Lerp(this.initialColor, this.finalColor, _percentageCompleted);
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public GameObject shooter;
+    private Transform _firePoint;
+
+    private void Awake() {
+        this._firePoint = this.transform.Find("FirePoint");
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        Invoke("Shoot", 1f);
+        Invoke("Shoot", 2f);
+        Invoke("Shoot", 3f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void Shoot() {
+        if (this.bulletPrefab != null && this.shooter != null && this._firePoint != null) {
+            GameObject myBullet = Instantiate(this.bulletPrefab, this._firePoint.position, Quaternion.identity) as GameObject;
+            Bullet bulletComponent = myBullet.GetComponent<Bullet>();
+            if (this.shooter.transform.localScale.x < 0f) {
+                bulletComponent.direction = Vector2.left;
+            }
+            else {
+                bulletComponent.direction = Vector2.right;
+            }
+        }
+    }
+}
