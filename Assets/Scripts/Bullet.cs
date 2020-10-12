4af45e40f7160bc880fd902cdf6353fcdc69401c
Moisés Vilar <mvilar@gradiant.org>
2020-10-12 13:18:43 +0200

    Curso 3, unidad 5: interacción con nuestro juego

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 2f;
+    public Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = this.direction.normalized * this.speed * Time.deltaTime;
+        // this.transform.position = new Vector2(this.transform.position.x + movement.x, this.transform.position.y + movement.y);
+        this.transform.Translate(movement);
+    }
+}
